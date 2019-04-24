c7ef0aca2f372849e43be3c8e143be6cff482f7a
Joe Barnes <joebarnes333@gmail.com>
2019-04-23 20:34:32 -0700

    add everything

---
+++ Program/Models/Leet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetSpeak
+{
+    class LeetSpeak
+    {
+        public string LeetSpeakTranslator(string word)
+        {
+            char[] wordArr = word.ToCharArray();
+
+            for(int i = 0; i < wordArr.Length; i++)
+            {
+                if(wordArr[i] == 'e')
+                {
+                    wordArr[i] = '3';
+                }
+                else if(wordArr[i] == 'o')
+                {
+                    wordArr[i] = '0';
+                }
+                else if(wordArr[i] =='I')
+                {
+                    wordArr[i] = '1';
+
+                }
+                else if(wordArr[i] =='t')
+                {
+                    wordArr[i] = '7';
+                }
+                else if(wordArr[i] =='s')
+                {
+                    wordArr[i] = 'Z';
+                }
+            }
+            string result = string.Join("", wordArr);
+            return result;
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetSpeak
+{
+    class Program
+    {
+        static void Main()
+        {
+            LeetSpeak word = new LeetSpeak();
+            Console.WriteLine("Enter a word: ");
+            string userOutput = Console.ReadLine();
+            string result = word.LeetSpeakTranslator(userOutput);
+            Console.WriteLine(result);
+            
+
+
+
+        }
+    }
+}
